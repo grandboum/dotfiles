8f3f2963825357813b0b4de1308111bee6a2f3d2
Illia Akhaiev <26223450+grandboum@users.noreply.github.com>
2025-02-19 10:43:37 +0000

    Added emacs config

---
+++ emacs/emacs.d/init.el
@@ -0,0 +1,17 @@
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(monokai))
+ '(custom-safe-themes
+   '("c7fd1708e08544d1df2cba59b85bd25263180b19b287489d4f17b9118487e718" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
